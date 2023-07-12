58785637f8c8ac1c8383977f7ea17aee190a5ac0
Tito Riccardo <titoriccardo@gmail.com>
2023-07-12 14:55:47 -0500

    carga del checkpoint

--- Simulación Challenge/checkpoint.py
+++ Simulación Challenge/checkpoint.py
@@ -12,7 +12,15 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if type(numero) != int:
+            return None
+    if numero < 1: 
+        return None
+    factorial = 1
+    while (numero > 1):
+        factorial = factorial * numero      #(factorial*=numero)
+        numero = numero - 1                 #(numero-=1)
+    return factorial
 
 def EsPrimo(valor):
     '''
@@ -26,7 +34,15 @@ def EsPrimo(valor):
         EsPrimo(8) debe retornar False
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if type(valor) != int:
+        return None
+
+    primo = True
+    for i in range(2,valor):
+        if (valor%i == 0):
+            primo = False
+            break
+    return primo
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +63,14 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    class Animal:
+        def __init__(self, especie, color):
+            self.edad = 0
+            self.especie = especie
+            self.color = color
+        
+        def CumplirAnios(self):
+            self.edad = self.edad + 1 #(self.edad += 1)
+            return self.edad
+        
+    return Animal(especie,color)
